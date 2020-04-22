581dae5898d6b08b7966546e79669cf4eaf78f8d
Chen <bakugan@gmail.com>
2020-04-22 11:49:18 +0300

    some changed

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import GenericForm from './GenericForm';
 import { signin } from '../actions/usersActions';
+import axios from 'axios';
 
 const Signin = (props) => {
+  const [data, setData] = useState('');
   console.log('signin working');
+  const check = async () => {
+    const res = await axios.get('http://localhost:5000/users/test');
+    console.log('res.data', res.data);
+    setData(res.data);
+  };
   return (
     <div style={{ margin: 30 }}>
       {/* <GenericForm email password action={signin} reDirect={props.history.push} /> */}
-      <button onClick={() => props.history.push('/at/home')}>Move</button>
+
+      <button onClick={() => check()}>Check</button>
+      {data && data.length > 0 && <h1>{data}</h1>}
       <h3>
         Dont have an account ? sign up <NavLink to='/at/signup'>Here</NavLink>
       </h3>
